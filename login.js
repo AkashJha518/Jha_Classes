bad90d131148b56dc2fa04dcdaab93a883108aed
AkashJha518 <rjha3474@gmail.com>
2023-02-12 17:54:25 +0530

    JCV0.04

--- login.js
+++ login.js
@@ -2,7 +2,31 @@ function validate()
 {
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
-    if(username=="admin" && password=="user")
+    if(username=="user1" && password=="123456")
+    {
+        window.location.assign("index.html");
+        alert("login succesfully");
+        return false;
+    }
+    else if(username=="user2" && password=="123456")
+    {
+        window.location.assign("index.html");
+        alert("login succesfully");
+        return false;
+    }
+    else if(username=="user3" && password=="123456")
+    {
+        window.location.assign("index.html");
+        alert("login succesfully");
+        return false;
+    }
+    else if(username=="user4" && password=="123456")
+    {
+        window.location.assign("index.html");
+        alert("login succesfully");
+        return false;
+    }
+    else if(username=="user5" && password=="123456")
     {
         window.location.assign("index.html");
         alert("login succesfully");
